45560e07ac718f866d3fe538347e983ea82553b9
Rocio Ortega <rocioortegapriego@gmail.com>
2018-10-14 19:53:38 +0200

    Use SimonSaysApplication add id to startGame button

--- app/src/main/java/pandiandcode/com/simonsays/GameFragment.kt
+++ app/src/main/java/pandiandcode/com/simonsays/GameFragment.kt
@@ -5,11 +5,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.koin.android.ext.android.inject
+import pandiandcode.com.game.GamePresenter
 
 /**
  * Created by Rocio Ortega on 14/10/2018.
  */
 class GameFragment : Fragment() {
+
+    val presenter: GamePresenter by inject()
+
     companion object {
         fun newInstance(): GameFragment = GameFragment()
     }
